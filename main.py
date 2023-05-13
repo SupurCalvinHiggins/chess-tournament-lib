fc0b650ba6e59f9ca410dfa10d5cea2fbc4e3b73
SupurCalvinHiggins <calvin@supurloop.com>
2023-05-13 13:03:32 -0400

    Created setup.py.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from text_gui import TextGui
-from unlimited_time_match import UnlimitedTimeMatch
-from random_engine import RandomEngine
+from chess_tournament.gui import TextGui
+from chess_tournament.match import UnlimitedTimeMatch
+from chess_tournament.engine import RandomEngine
 
 
 if __name__ == "__main__":
     white_engine = RandomEngine()
     black_engine = RandomEngine()
-    match = UnlimitedTimeMatch(white_engine=white_engine, black_engine=black_engine)
+    match = UnlimitedTimeMatch(white_engine, black_engine)
     gui = TextGui(match)
     gui.start()
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+      name="chess tournament",
+      version="0.1",
+      description="Some utilities for simple chess tournaments with the python-chess package.",
+      author="Calvin Higgins",
+      author_email="calvin_higgins2@uri.edu",
+      package_dir={"": "src"},
+      packages=setuptools.find_packages(where="src"),
+      python_requires=">=3.9",
+      install_requires=["python-chess"],
+)
