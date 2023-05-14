f0b8bc0d224eb1172f443afe5ef166a8a2dabf95
Ethan Carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2023-05-14 16:52:12 -0400

    Added InputEngine to __init__.py

--- src/chess_tournament/engine/__init__.py
+++ src/chess_tournament/engine/__init__.py
@@ -2,3 +2,4 @@ from .base_engine import BaseEngine
 from .checked_engine import CheckedEngine
 from .random_engine import RandomEngine
 from .engine_result import EngineResult
+from .input_engine import InputEngine
